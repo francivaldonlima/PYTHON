dbf831a53c5bb6ad055606f577784ab1aecb33b5
francivaldonlima <francivaldonlima@gmail.com>
2021-10-20 03:25:53 -0300

    Update tradutorterminal.py

--- tradutorterminal.py
+++ tradutorterminal.py
@@ -2,6 +2,6 @@
 # um tradutor usando do tradutor do linux translate-shell
 #https://www.soimort.org/translate-shell/
 import os
-f = os.popen('trans -b ""all')
+f = os.popen('trans -b "all"')
 now = f.read()
 print (now)
