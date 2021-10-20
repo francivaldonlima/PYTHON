0ffc740fbc31ad4efc1778296662e272feb17707
francivaldonlima <francivaldonlima@gmail.com>
2021-10-20 03:25:18 -0300

    Create tradutorterminal.py
    traduzindo usando o tradutor do terminal

---
+++ tradutorterminal.py
@@ -0,0 +1,7 @@
+#coding: utf-8
+# um tradutor usando do tradutor do linux translate-shell
+#https://www.soimort.org/translate-shell/
+import os
+f = os.popen('trans -b ""all')
+now = f.read()
+print (now)
